d7ca1e7d18c15f9db502eecef8f395c63605ce2f
Nikodem Stach <nikodem.stach@outlook.com>
2024-01-25 20:35:31 +0100

    fix: added gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/todo-react-ts/',
+  plugins: [react()],
 });
